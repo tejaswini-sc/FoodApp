9e0211d45816baabbd53999961a5791a2d4e209e
tejaswini-sc <tchilakanti@gmail.com>
2026-06-22 23:11:31 +0530

    cart changed

--- src/main/java/com/foodapp/model/Cart.java
+++ src/main/java/com/foodapp/model/Cart.java
@@ -1,108 +1,18 @@
 package com.foodapp.model;
 
-import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Cart {
 
-	private int cartId;
-
-	private int userId;
-
-	private int menuId;
-
-	private int quantity;
-
-	private Timestamp addedDate;
+	Map<Integer,CartItem> items;
 	
-	public Cart() {
-		// TODO Auto-generated constructor stub
-	}
+		public Cart() {
+			
+			items=new HashMap<Integer,CartItem>();
+		}
+		public Map<Integer,CartItem> getItems(){
+			return items;
+		}
 
-	public Cart(int cartId, int userId, int menuId, int quantity, Timestamp addedDate) {
-		super();
-		this.cartId = cartId;
-		this.userId = userId;
-		this.menuId = menuId;
-		this.quantity = quantity;
-		this.addedDate = addedDate;
-	}
-
-	/**
-	 * @return the cartId
-	 */
-	public int getCartId() {
-		return cartId;
-	}
-
-	/**
-	 * @param cartId the cartId to set
-	 */
-	public void setCartId(int cartId) {
-		this.cartId = cartId;
-	}
-
-	/**
-	 * @return the userId
-	 */
-	public int getUserId() {
-		return userId;
-	}
-
-	/**
-	 * @param userId the userId to set
-	 */
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
-	/**
-	 * @return the menuId
-	 */
-	public int getMenuId() {
-		return menuId;
-	}
-
-	/**
-	 * @param menuId the menuId to set
-	 */
-	public void setMenuId(int menuId) {
-		this.menuId = menuId;
-	}
-
-	/**
-	 * @return the quantity
-	 */
-	public int getQuantity() {
-		return quantity;
-	}
-
-	/**
-	 * @param quantity the quantity to set
-	 */
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	/**
-	 * @return the addedDate
-	 */
-	public Timestamp getAddedDate() {
-		return addedDate;
-	}
-
-	/**
-	 * @param addedDate the addedDate to set
-	 */
-	public void setAddedDate(Timestamp addedDate) {
-		this.addedDate = addedDate;
-	}
-
-	@Override
-	public String toString() {
-		return "Cart [cartId=" + cartId + ", userId=" + userId + ", menuId=" + menuId + ", quantity=" + quantity
-				+ ", addedDate=" + addedDate + "]";
-	}
-	
-	
-	
 }
---
+++ src/main/java/com/foodapp/model/CartItem.java
@@ -0,0 +1,90 @@
+package com.foodapp.model;
+
+public class CartItem {
+
+	private int menuId;
+	private int restaurantId;
+	private String name;
+	private float price;
+	private int quantity;
+	
+	public CartItem(int menuId, int restaurantId, String name, float price, int quantity) {
+		super();
+		this.menuId = menuId;
+		this.restaurantId = restaurantId;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the menuId
+	 */
+	public int getMenuId() {
+		return menuId;
+	}
+
+	/**
+	 * @param menuId the menuId to set
+	 */
+	public void setMenuId(int menuId) {
+		this.menuId = menuId;
+	}
+
+	/**
+	 * @return the restaurantId
+	 */
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+
+	/**
+	 * @param restaurantId the restaurantId to set
+	 */
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public float getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+		
+}
